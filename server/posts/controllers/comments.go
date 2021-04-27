770430db48a816c2c2794ed8f952f3510f236b4a
Haizza1 <darioroman2001@gmail.com>
2021-04-27 19:24:23 -0400

    cretae controllers for likes and comments.

---
+++ server/posts/controllers/comments.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"blogv2/posts/models"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type CommentsController struct{}
+
+func (c *CommentsController) AddComment(comment *models.Comment, db *gorm.DB) *echo.HTTPError {
+	if err := db.Create(&comment).Error; err != nil {
+		return echo.NewHTTPError(500, "unable to create post")
+	}
+
+	return nil
+}
+
+func (c *CommentsController) GetPostComments(postId, limit int, cursor *string, db *gorm.DB) ([]*models.Comment, bool) {
+	var comments []*models.Comment
+	if limit > 50 {
+		limit = 50
+	}
+	limit++
+
+	if cursor != nil {
+		db.Raw(`
+			SELECT c.*,
+			FROM comments c
+			WHERE post_id = ?
+			AND c.created_at < ?
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, postId, cursor, limit).Find(&comments)
+	} else {
+		db.Raw(`
+			SELECT p.*,
+			FROM comments c
+			WHERE post_id = ?
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, postId, limit).Find(&comments)
+	}
+
+	if len(comments) == 0 {
+		return nil, false
+	}
+	if len(comments) == limit {
+		return comments[0 : limit-1], true
+	}
+
+	return comments[0 : len(comments)-1], false
+}
+
+func (c *CommentsController) DeleteComment(id, userId int, db *gorm.DB) *echo.HTTPError {
+	tx := db.Where("id = ? AND user_id = ?", id, userId).Delete(&models.Comment{})
+	if tx.RowsAffected == 0 || tx.Error != nil {
+		return echo.NewHTTPError(400, "post does not exists or you are not the owner")
+	}
+
+	return nil
+}
--- server/posts/controllers/posts.go
+++ server/posts/controllers/posts.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"blogv2/posts/models"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -35,4 +36,138 @@ func (p *PostsController) DeletePost(id int, userID int, db *gorm.DB) *echo.HTTP
 	}
 
 	return nil
+}
+
+func (p *PostsController) GetPosts(limit int, cursor *string, userId int, db *gorm.DB) ([]models.Post, bool) {
+	var posts []models.Post
+	if limit > 50 {
+		limit = 50
+	}
+	limit++
+
+	if cursor != nil {
+		db.Raw(`
+			SELECT p.*,
+			( SELECT "value" from "likes" 
+			WHERE "user_id" = ? and "post_id" = p.id) as "StateValue"
+			FROM posts p
+			WHERE p.created_at < ?
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, userId, cursor, limit).Find(&posts)
+	} else {
+		db.Raw(`
+			SELECT p.*,
+			( SELECT "value" from "updoots" 
+			WHERE "user_id" = ? and "post_id" = p.id) as "StateValue"
+			FROM posts p
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, userId, limit).Find(&posts)
+	}
+	if len(posts) == 0 {
+		return nil, false
+	}
+	if len(posts) == limit {
+		return posts[0 : limit-1], true
+	}
+
+	return posts[0 : len(posts)-1], false
+}
+
+func (p *PostsController) SetLike(postId, userId, value int, db *gorm.DB) bool {
+	var like models.Like
+	isLike := value != -1
+	var realValue uint16
+
+	if isLike {
+		realValue = 1
+	} else {
+		realValue = 0
+	}
+
+	db.Table("likes").Where("user_id = ? and post_id = ?", userId, postId).Find(&like)
+
+	// user is vote the post before and
+	// they are changing their vote
+	if like.PostID != 0 && like.Value != realValue {
+		query := fmt.Sprintf(`
+			START TRANSACTION;
+
+			UPDATE "likes"
+			SET value = %d
+			WHERE post_id = %d AND user_id = %d; 
+
+			UPDATE "posts"
+			SET Likes = Likes - 1
+			WHERE posts.id = %d;
+			
+			COMMIT;
+		`, realValue, postId, userId, postId)
+
+		if err := db.Exec(query).Error; err != nil {
+			return false
+		}
+
+		// user has never voted before
+	} else if like.PostID == 0 {
+		query := fmt.Sprintf(`
+			START TRANSACTION;
+
+			INSERT INTO "likes" ("user_id", "post_id", "value")
+			values(%d, %d, %d);
+
+			UPDATE "posts"
+			SET Likes = Likes + %d
+			WHERE posts.id = %d;
+
+			COMMIT;
+		`, userId, postId, realValue, realValue, postId)
+
+		if err := db.Exec(query).Error; err != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (p *PostsController) GetUserPosts(limit, userId, profileId int, cursor *string, db *gorm.DB) ([]*models.Post, bool) {
+	var posts []*models.Post
+
+	if limit > 50 {
+		limit = 50
+	}
+	limit++
+
+	if cursor != nil {
+		db.Raw(`
+			SELECT p.*,
+			( SELECT "value" from "likes" 
+			WHERE "user_id" = ? and "post_id" = p.id) as "StateValue"
+			FROM posts p
+			WHERE user_id = ?
+			AND p.created_at < ?
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, userId, profileId, cursor, limit).Find(&posts)
+	} else {
+		db.Raw(`
+			SELECT p.*,
+			( SELECT "value" from "updoots" 
+			WHERE "user_id" = ? and "post_id" = p.id) as "StateValue"
+			FROM posts p
+			WHERE user_id = ?
+			ORDER BY p.created_at DESC
+			LIMIT ?
+		`, userId, profileId, limit).Find(&posts)
+	}
+	if len(posts) == 0 {
+		return nil, false
+	}
+	if len(posts) == limit {
+		return posts[0 : limit-1], true
+	}
+
+	return posts[0 : len(posts)-1], false
 }
