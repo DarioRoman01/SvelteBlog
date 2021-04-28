b11ccaffce8848370cdf53769a597e6f6a37ec2f
Haizza1 <darioroman2001@gmail.com>
2021-04-28 09:19:05 -0400

    added all views to the routes file.

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"blogv2/db"
+	pModels "blogv2/posts/models"
+	pViews "blogv2/posts/views"
 	"blogv2/users/models"
 	"blogv2/users/views"
 	"log"
@@ -16,9 +18,18 @@ func SetRoutes(e *echo.Echo) {
 		log.Fatal("unable to connect to postgres: ", err)
 	}
 
-	psql.AutoMigrate(&models.User{}, &models.Profile{})
+	psql.AutoMigrate(
+		&models.User{},
+		&models.Profile{},
+		&pModels.Post{},
+		&pModels.Comment{},
+		&pModels.Like{},
+	)
+
 	usersViews := &views.UsersViews{DB: psql}
 	profileViews := &views.ProfileViews{DB: psql}
+	postsViews := &pViews.PostsViews{DB: psql}
+	commentViews := &pViews.CommentsViews{DB: psql}
 
 	e.Use(CORSconfig())
 	e.Use(JwtMiddleware())
@@ -33,4 +44,17 @@ func SetRoutes(e *echo.Echo) {
 	e.PATCH("/profile/:id", profileViews.UpdateProfileView)
 	e.POST("/profile", profileViews.CreateProfileView)
 	e.GET("/profile/:username", profileViews.GetProfileView)
+	e.GET("/profile/:id/posts", postsViews.GetUserPostsView)
+	e.POST("/profile/:id/follow", profileViews.FollowView)
+	e.POST("/profile/:id/unfollow", profileViews.UnFollowView)
+
+	e.GET("/posts", postsViews.GetPostsView)
+	e.POST("/posts", postsViews.CreatePostView)
+	e.GET("/posts/:id", postsViews.GetPostView)
+	e.DELETE("/posts/:id", postsViews.DeletePostView)
+	e.POST("/posts/:id/like", postsViews.ToggleLikeView)
+	e.POST("/posts/:id/comments", commentViews.AddCommentView)
+	e.GET("/posts/:id/comments", commentViews.GetPostCommentsView)
+	e.DELETE("/posts/:id/comments", commentViews.DeleteCommentView)
+
 }
