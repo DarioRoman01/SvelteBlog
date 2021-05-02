022f1c7d120ac362f0b2ae76c26fb0e787e52664
Haizza1 <darioroman2001@gmail.com>
2021-05-02 11:34:04 -0400

    edit comments components.

--- web/src/components/Comments.svelte
+++ web/src/components/Comments.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-  import type { PaginatedComments } from "../requests/comments";
+  import { 
+    MaterialApp, 
+    Button, 
+    ProgressCircular, 
+    Icon,
+    Overlay 
+  } from "svelte-materialify";
+  import type { Comment, PaginatedComments } from "../requests/comments";
+  import CreateCommentModal from "./CreateCommentModal.svelte";
   import CommentsCard from "./CommentsCard.svelte"
-  import { MaterialApp, Button, ProgressCircular, Icon } from "svelte-materialify";
   import { mdiPlusCircle } from "@mdi/js"
   import { api } from "../requests/users";
-  import { onMount } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
 
   let comments: PaginatedComments;
   export let id: number;
+  export let showModal: boolean;
+  export let currentUserId: number;
+  const dispactch = createEventDispatcher();
 
   onMount(async () => {
     comments = await api<PaginatedComments>(`/posts/${id}/comments?limit=10`);
   });
+
+  const refetch = async () => {
+    const newComments = await api<PaginatedComments>(`/posts/${id}/comments?limit=10`);
+    comments.comments = newComments.comments;
+    showModal = false;
+  }
+
 </script>
 
 <MaterialApp>
@@ -23,7 +40,11 @@
     {:else}
       {#each comments.comments as comment}
         <div class="d-flex flex-column">
-          <CommentsCard comment={comment}/>
+          <CommentsCard 
+            comment={comment}
+            currentUserId={currentUserId}
+            on:delete={() => refetch()}
+          />
         </div>
       {/each}
       {#if comments.hasMore === true}
@@ -34,6 +55,16 @@
         </div>
       {/if}
     {/if}
+  <Overlay active={showModal}>
+    <CreateCommentModal 
+      postId={id}
+      on:create={() => refetch()}
+      on:close={() => {
+        showModal = false
+        dispactch("closeModal");
+      }}
+    />
+  </Overlay>
   {:else}
     <ProgressCircular size={50} indeterminate color="pink lighten-3" />
   {/if}
--- web/src/components/CommentsCard.svelte
+++ web/src/components/CommentsCard.svelte
@@ -5,24 +5,36 @@
     CardActions,
     Button,
     MaterialApp,
+    Icon
   } from "svelte-materialify";
   import type { Comment } from "../requests/comments"
+  import { mdiTrashCan } from "@mdi/js"
+import { createEventDispatcher } from "svelte";
 
   export let comment: Comment;
+  export let currentUserId: number;
+  const dispactch = createEventDispatcher();
+  const handleDelete = () => {
+    dispactch("delete");
+  }
 </script>
 
 <MaterialApp>
-  <div class="mt-4 mb-4">
-    <Card style="width:500px;">
+    <Card raised style="width:50em;">
+      <div class="mt-4 mb-4 d-flex justify-space-between">
       <CardText>
         <div>{comment.creator}</div>
         <div class="text--primary">
           {comment.body}
         </div>
       </CardText>
-      <CardActions>
-        <Button text class="primary-text">see More</Button>
-      </CardActions>
+      {#if currentUserId === comment.creatorId}
+        <CardActions>
+          <Button icon class="pink-text">
+            <Icon path={mdiTrashCan} on:click={() => handleDelete}/>
+          </Button>
+        </CardActions>
+      {/if}
+      </div>
     </Card>
-  </div>
 </MaterialApp>
