b58fbdd633859c29cf64915780d7f92d32c6eae6
Haizza1 <66578210+Haizza1@users.noreply.github.com>
2021-05-02 18:02:34 -0400

    Delete server directory

--- server/posts/models/likes.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// like model
-// the value only can be 1 or 0
-type Like struct {
-	UserID uint `gorm:"primaryKey"`
-	PostID uint `gorm:"primaryKey"`
-	Value  uint16
-}
--- server/users/models/errors.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// ErrorMessage model for users signup and login
-type ErrorMessage struct {
-	Field   string `json:"field"`
-	Message string `json:"message"`
-}
