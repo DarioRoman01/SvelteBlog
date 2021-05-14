676d461f0a36d63982d0280ffb6101ce4a548f52
Haizza1 <darioroman2001@gmail.com>
2021-05-14 10:42:28 -0400

    added pagination in the frontend.

--- web/src/components/Comments.svelte
+++ web/src/components/Comments.svelte
@@ -1,34 +1,48 @@
 <script lang="ts">
-  import { 
-    MaterialApp, 
-    Button, 
-    ProgressCircular, 
+  import {
+    MaterialApp,
+    Button,
+    ProgressCircular,
     Icon,
-    Overlay 
+    Overlay,
   } from "svelte-materialify";
   import type { Comment, PaginatedComments } from "../requests/comments";
   import CreateCommentModal from "./CreateCommentModal.svelte";
-  import CommentsCard from "./CommentsCard.svelte"
-  import { mdiPlusCircle } from "@mdi/js"
+  import CommentsCard from "./CommentsCard.svelte";
+  import { mdiPlusCircle } from "@mdi/js";
   import { api } from "../requests/users";
   import { createEventDispatcher, onMount } from "svelte";
 
+  const dispactch = createEventDispatcher();
   let comments: PaginatedComments;
+  let lastComment: Comment;
+
   export let id: number;
   export let showModal: boolean;
   export let currentUserId: number;
-  const dispactch = createEventDispatcher();
 
   onMount(async () => {
     comments = await api<PaginatedComments>(`/posts/${id}/comments?limit=10`);
+    lastComment = comments.comments[comments.comments.length - 1];
   });
 
   const refetch = async () => {
-    const newComments = await api<PaginatedComments>(`/posts/${id}/comments?limit=10`);
+    const newComments = await api<PaginatedComments>(
+      `/posts/${id}/comments?limit=10`
+    );
     comments.comments = newComments.comments;
     showModal = false;
-  }
+  };
 
+  const loadMore = async (lastComment: Comment) => {
+    const newComments = await api<PaginatedComments>(
+      `/posts/${id}/comments?limit=10&cursor=${lastComment.createdAt}`
+    );
+
+    newComments.comments.forEach((comment) => comments.comments.push(comment));
+    comments.hasMore = newComments.hasMore;
+    lastComment = comments.comments[comments.comments.length - 1];
+  };
 </script>
 
 <MaterialApp>
@@ -40,31 +54,27 @@
     {:else}
       {#each comments.comments as comment}
         <div class="d-flex flex-column">
-          <CommentsCard 
-            comment={comment}
-            currentUserId={currentUserId}
-            on:delete={() => refetch()}
-          />
+          <CommentsCard {comment} {currentUserId} on:delete={() => refetch()} />
         </div>
       {/each}
       {#if comments.hasMore === true}
         <div class="d-flex justify-center mt-4 mb-3">
-          <Button class="pink lighten-3">
-            <Icon path={mdiPlusCircle}/>
+          <Button class="pink lighten-3" on:click={() => loadMore(lastComment)}>
+            <Icon path={mdiPlusCircle} />
           </Button>
         </div>
       {/if}
     {/if}
-  <Overlay active={showModal}>
-    <CreateCommentModal 
-      postId={id}
-      on:create={() => refetch()}
-      on:close={() => {
-        showModal = false
-        dispactch("closeModal");
-      }}
-    />
-  </Overlay>
+    <Overlay active={showModal}>
+      <CreateCommentModal
+        postId={id}
+        on:create={() => refetch()}
+        on:close={() => {
+          showModal = false;
+          dispactch("closeModal");
+        }}
+      />
+    </Overlay>
   {:else}
     <ProgressCircular size={50} indeterminate color="pink lighten-3" />
   {/if}
--- web/src/components/Posts.svelte
+++ web/src/components/Posts.svelte
@@ -7,14 +7,23 @@
   } from "svelte-materialify";
   import { onMount } from "svelte";
   import PostCard from "./PostCard.svelte";
-  import type { PaginatedPosts } from "../requests/posts";
+  import type { PaginatedPosts, Post } from "../requests/posts";
   import { api } from "../requests/users";
   import { mdiPlusCircle } from "@mdi/js";
 
+  let lastPost: Post;
   let posts: PaginatedPosts;
   onMount(async () => {
     posts = await api<PaginatedPosts>("/posts?limit=10")
+    lastPost = posts.posts[posts.posts.length - 1];
   });
+
+  const loadMore = async (lastPost: Post) => {
+    const newPosts = await api<PaginatedPosts>(`/posts?limit=10&cursor=${lastPost.createdAt}`);
+    newPosts.posts.forEach((post) => posts.posts.push(post));
+    posts.hasMore = newPosts.hasMore;
+    lastPost = posts.posts[posts.posts.length - 1];
+  }
 </script>
 
 <MaterialApp>
@@ -26,7 +35,7 @@
     {/each}
     {#if posts.hasMore == true}
       <div class="d-flex justify-center mt-4 mb-3">
-        <Button class="pink lighten-3">
+        <Button class="pink lighten-3" on:click={() => loadMore(lastPost)}>
           <Icon path={mdiPlusCircle}/>
         </Button>
       </div>
